862f8b1cf0cfab0c405aca7b3b59bcc793d08bf3
juliohdsv <ricksv92@gmail.com>
2023-10-04 16:22:37 -0300

    upload project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Home(){
+    return(
+        <div className="container">
+            <form action="">
+                <input type="email" placeholder="E-mail"/>
+                <input type="password" placeholder="Senha"/>
+                <button type="submit">Salvar</button>
+            </form>
+        </div>
+    );
+}
